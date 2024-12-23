9bdeacbc3283b0fc70a6f2dce7a9dca2d1a8860c
ankitdabhi001 <a1000aje@gmail.com>
2024-12-23 18:40:04 +0530

    afea

---
+++ bbm/about.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+   <style>
+    
+    .container img{
+
+       
+    width:100%;
+    height:100%;
+    background-size: 100% 100%;
+    }
+
+    
+   </style>
+ 
+</head>
+<body>
+<h2 class="logo"><img src="download.png"><span>B</span>oat<span>B</span>ooking</h2>
+            <nav>   
+          
+            <ul>
+                <li><a href="navbar.html">HOME</a></li>
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">BOOKING STATUS</a></li>
+                <li><a href="#">CONTACT</a></li>
+                <li><a href="#">ADMIN</a></li> 
+				    <    class="hero">
+                    </div>
+                </ul>
+            </nav>
+<link rel="stylesheet" href="navbar.html">
+   <div class="container">
+   <img src="images/boat4.jpg">
+</div>
+
+    <footer class="foot">
+        <h4>This is Footer</h4>
+        <h4>Welcome To Our Website</h4>
+        </footer>
+</body>
+</html>
+
---
+++ bbm/contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<nav>   
+           <h2 class="logo"><!--<img src="images/logo.png">--><span>B</span>oat<span> B</span>ooking </h2>
+            <ul>
+                <li><a href="http://localhost/myproject/bbm/navbar.html">HOME</a></li>
+
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">BOOKING STATUS</a></li>
+                <li><a href="#">CONTACT</a></li>
+                <li><a href="#">ADMIN</a></li>
+				 <li><a href="#">help</a></li>
+               
+		
+				    <div class="hero">
+                    </div>
+                </ul>
+            </nav>
+            contact
+          
+        <footer class="foot">
+        <h4>This is Footer</h4>
+        <h4>Welcome To Our Website</h4>
+        </footer> 
+</body>
+</html>
