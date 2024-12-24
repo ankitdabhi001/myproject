7a23ea5461b82082017d2ed09bc18c301af49026
ankitdabhi001 <a1000aje@gmail.com>
2024-12-24 19:48:22 +0530

    eee

---
+++ bbm/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+	<link rel="stylesheet" type="text/css" href="log.css">
+</head>
+<body>
+
+    <div id="form">
+        <h1>login form</h1>
+        <form name="form" method="post">
+            <label>username:</label>
+            <input type="text" id="user" name="user"/></br></br>
+            <label>password:</label>
+            <input type="password" id="pass" name="pass"/></br></br>
+            <input type="submit" id="btn" value="login" name="submit"/>
+        </form>
+</div>
+
+</body>
+</html>
