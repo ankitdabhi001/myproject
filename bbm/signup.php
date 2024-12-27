2d82a6bc4b0992aa782f56db9f3c18e588212b85
ankitdabhi001 <a1000aje@gmail.com>
2024-12-27 21:38:27 +0530

    signup

--- bbm/signup.php
+++ bbm/signup.php
@@ -1,11 +1,52 @@
 <?php
  $conn = new mysqli("localhost", "root", "", "boat");
- 
+ /*
  if($conn->connect_error)
 {
 die("connection failed".$conn->connect_error);
 }
-echo"connection success";
+echo"connection success"; */
+ 
+ if (isset($_POST["signup"]))
+	 {
+    $username=$_POST["name"];
+	
+	$phone=$_POST["phone"];
+	
+	$email=$_POST["email"];
+	
+    $pass=$_POST["password"];
+	
+	$confirm=$_POST["confirm"];
+	
+	$gender=$_POST["gender"]; 
+    
+	
+	$check_email="select * from sign where email='$email'";
+	$check=$conn->query($check_email);
+	
+	$q="INSERT INTO  sign (username,phone, email, pass,confirm) VALUES ('$username',$phone, '$email', '$pass','$confirm','$gender')"; 
+	$qu=mysqli_query($conn,$q);
+	
+	
+				if($check->num_rows>0)
+				{
+					echo "<script>alert('this email is alredy taken please select another email')</script>";
+				}
+				else
+				{
+					$q="INSERT INTO sign (username, phone, email, pass, confirm, gender) VALUES ('$username',$phone, '$email', '$pass','$confirm','$gender')"; 
+				}
+				
+				 if ($conn->query($q)===TRUE)
+					 {
+						echo $success_message = "New record created successfully. You can now log in.";
+					} 
+					else {
+						 "Error: " . $conn->error;
+							}
+	 
+	 }
  
 ?>
 
@@ -42,21 +83,18 @@ echo"connection success";
 			
 			 <div class="form-group">
                 <label for="re-pass">Confirm Password</label>
-                <input type="password" id="password" name="password" required>
+                <input type="password" id="password" name="confirm" required>
             </div>
 			
 			 <div class="form-group">
                 <label for="gender">Select Gender</label></br>
-                <input type="radio" id="male" name="male" value="Male"required>
-				<label for="gender">Male</label>
-				
-				<input type="radio" id="female" name="female" value="female"required>
-				<label for="gender">Female</label>
+                <input type="radio" id="gen" name="gender" value="Male"required><label id="ma" for="gender">Male</label>
+				<input type="radio" id="gen" name="gender" value="female"required><label id="ma" for="gender">Female</label>
 
-            </div>
+            </div> 
 			
             <div class="form-group">
-                <button type="submit" class="submit-btn">Sign Up</button>
+                <button type="submit" name="signup" class="submit-btn">Sign Up</button>
             </div>
         </form>
         <p>Already Have An Account? <a href="login.php">Log In</a></p>
